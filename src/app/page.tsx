bb7150086ff3d9f0ea492040845fc62cde0e2a80
Jetus <68517044+j3tus@users.noreply.github.com>
2024-09-01 15:25:07 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,41 @@
-"use client"
-import Footer from './components/footer';
-import { FaGithub, FaDiscord, FaDev } from 'react-icons/fa';
-import { NextUIProvider } from "@nextui-org/react";
+import React from 'react';
 import { Tooltip } from "@nextui-org/tooltip";
+import { FaGithub, FaDiscord } from 'react-icons/fa';
+import Footer from './components/footer';
+
+const SocialLink = ({ href, icon: Icon, tooltip }) => (
+  <Tooltip content={tooltip} placement="bottom">
+    <a
+      href={href}
+      className="mx-3 hover:scale-125 transition-transform shadow-md rounded-lg"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Icon className="text-gray-400 text-5xl" />
+    </a>
+  </Tooltip>
+);
 
 export default function Home() {
+  const socialLinks = [
+    { href: 'https://github.com/j3tus', icon: FaGithub, tooltip: 'J3tus' },
+    { href: 'https://discord.com/users/923175161350455336', icon: FaDiscord, tooltip: 'JetusW' },
+  ];
+
   return (
-    <NextUIProvider>
-      <main className='overflow-hidden relative'>
-        <div className='stars'></div>
-        <div className='flex flex-col items-center justify-center h-screen '>
-        <div className='mb-24 md:mb-0'> 
-          <h1 className='text-6xl font-bold text-white '>Jetus</h1>
-            <div className='flex justify-center mt-5'>
-              <Tooltip content="J3tus" placement="bottom">
-                <a
-                  href='https://github.com/j3tus'
-                  className='mx-3 hover:scale-125 transition-transform shadow-md rounded-lg'
-                >
-                  <FaGithub className='text-gray-400 text-5xl' />
-                </a>
-              </Tooltip>
-              <Tooltip content="JetusW" placement="bottom" key="primary">
-                <a
-                  href='https://discord.com/users/923175161350455336'
-                  className='mx-3 hover:scale-125 transition-transform shadow-md rounded-lg'
-                >
-                  <FaDiscord className='text-gray-400 text-5xl' />
-                </a>
-              </Tooltip>
+      <main className="relative h-screen overflow-hidden">
+        <div className="stars absolute inset-0" />
+        <div className="flex flex-col items-center justify-center h-full">
+          <div className="mb-24 md:mb-0 text-center"> 
+            <h1 className="text-6xl font-bold text-white mb-5">Jetus</h1>
+            <div className="flex justify-center">
+              {socialLinks.map((link, index) => (
+                <SocialLink key={index} {...link} />
+              ))}
             </div>
           </div>
         </div>
         <Footer />
       </main>
-    </NextUIProvider>
   );
 }
